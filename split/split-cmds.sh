7a88764bb76b4c41fd1b6c792c2e4f5cda406a6a
sinkovit <sinkovit@sdsc.edu>
2024-01-19 13:40:50 -0800

    Adds comments

--- split/split-cmds.sh
+++ split/split-cmds.sh
@@ -1,3 +1,4 @@
+# Look in the split directory to see results of splitting FASTA file
 split genome.fasta
 split -l 2000 genome.fasta genome_v1_
 split -l 200 -t '>' genome.fasta genome_v2_ # Does not work on MacOS
