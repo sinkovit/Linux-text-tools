37af5f55dd6bc7ce6eb88009c8da8caea973195b
Robert Sinkovits <sinkovit@sdsc.edu>
2025-04-26 13:20:06 -0700

    Update awk-cmds.sh

--- awk/awk-cmds.sh
+++ awk/awk-cmds.sh
@@ -10,3 +10,5 @@ awk '{sin1=sin($1); cos2=cos($2); print $1, $2, sin1, cos2}' numbers.txt
 awk '{log1=log($1); sqrt2=sqrt($2); print $1, $2, log1, sqrt2}' numbers.txt
 awk 'BEGIN {PI=3.14159; print "  x    y   sin(x)  cos(y)"} {sin1=sin($1*PI); cos2=cos($2*PI); printf "%4.1f %4.1f %7.4f %7.4f", $1, $2, sin1, cos2; print ""}' numbers.txt
 awk '{s1+=$1; s2+=$2; print} END {print s1/NR, s2/NR}' numbers.txt
+awk '{if (NF>2) print}' ragged.txt
+awk '{if ($1 > $2) print $2, $1; else print $1, $2}' numbers.txt 
